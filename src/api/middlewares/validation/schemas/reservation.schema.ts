15769dfe0cd61321c94e66e1e9a23106acf5fc1e
Oyedeji Peace <oyedejipeace@gmail.com>
2020-09-06 00:40:34 +0100

    revamp reservation api

--- src/api/middlewares/validation/schemas/reservation.schema.ts
+++ src/api/middlewares/validation/schemas/reservation.schema.ts
@@ -3,7 +3,9 @@ import Joi from '@hapi/joi'
 const date = Joi.date().required()
 const stripeToken = Joi.string().trim().required()
 const persons = Joi.number().integer().min(1).max(6).required()
-const time = Joi.string().regex(/^([0-9]{2}):([0-9]{2})$/).required()
+const time = Joi.string()
+  .regex(/^([01]\d|2[0-3]):?([0-5]\d)$/)
+  .required()
 const type = Joi.string().trim().valid(['breakfast', 'lunch', 'dinner', 'drinks']).required()
 
 export const createReservationSchema = Joi.object().keys({
